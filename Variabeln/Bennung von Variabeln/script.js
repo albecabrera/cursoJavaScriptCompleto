3081886ec6906430c9228ce67057e11cd3e70e97
albecabrera <albe.cabrera@yahoo.de>
2024-06-07 22:05:27 +0200

    commit -m "cambio2"

---
+++ Variabeln/Bennung von Variabeln/script.js
@@ -0,0 +1,21 @@
+"user strict";
+
+let variable = "Meine Variable";
+console.log(variable);
+
+//Regel 1: keine Leerzeichen
+//Regel 2: keine Umlaute
+//Regel 3: keine Sonderzeichen
+// Regel 4: erstes Zeichen darf nicht eine Zahl sein/ immer Buschstaben
+// Regel 5: Gross- und Kleinschreibung ist erlaubt (case-sensitive)
+let name = "Alberto";
+let Name = "Alberto";
+
+console.log(name);
+console.log(Name);
+
+
+//statt Leerzeichen _ oder CamelCase-Schreibweise verwenden. 
+
+// Regel 6: keine Keywords als Variablennamen.
+let let = "Mein Fehler";
