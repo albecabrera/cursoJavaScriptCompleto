1c474bdaf1a1b4c9dad69a95a067177a22b15141
albecabrera <albe.cabrera@yahoo.de>
2024-06-07 18:41:54 +0200

    commit -m "cambio nuevo"

--- !starter-template/main.js
+++ !starter-template/main.js
@@ -1,41 +1,6 @@
 "use strict"; // Dies benutzt man als Standard, damit die Ausgabe in der Konsole angezeigt wird. Es wird eine Warnung angezeigt.
 
-console.log("Hallo Welt!");
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+console.log("Hallo Dennis!");
+console.log("Hallo Ruben!");
+console.log("Wie geht es dir!");
 
---
+++ Variabeln/Variabeln in JS/main.js
@@ -0,0 +1,9 @@
+"use strict";
+
+let stadt = "Paderborn";
+let alter = 27;
+let test = "Alberto Cabrera";
+
+console.log(stadt);
+console.log(alter);
+console.log(test);
