70cad0d29a88563d8c03a48176a269aaa1cc4b85
albecabrera <albe.cabrera@yahoo.de>
2024-06-10 13:15:47 +0200

    commit -m "change 3"

--- Template-Strings+und+Operatoren+einbauen/src/js/script.js
+++ Template-Strings+und+Operatoren+einbauen/src/js/script.js
@@ -1,21 +1,21 @@
 "use strict";
 
 // Eingabedaten holen
-let titel_1 = prompt("Titel:");
+let titel_1 = prompt("Titel 1");
 console.log(`Titel: ${titel_1}`);
-let typ_1 = prompt("Typ (Einnahme oder Ausgabe):");
+let typ_1 = prompt("Typ (Einahme oder Ausgabe)");
 console.log(`Typ: ${typ_1}`);
-let betrag_1 = parseInt(prompt("Betrag (in Cent):"));
+let betrag_1 = prompt("Betrag (in Cent):");
 console.log(betrag_1);
 console.log(`Betrag: ${betrag_1} ct`);
 let datum_1 = prompt("Datum (jjjj-mm-tt):");
 console.log(`Datum: ${datum_1}`);
 
-let titel_2 = prompt("Titel:");
+let titel_2 = prompt("Titel 2");
 console.log(`Titel: ${titel_2}`);
-let typ_2 = prompt("Typ (Einnahme oder Ausgabe):");
-console.log(`Typ: ${typ_2}`);
-let betrag_2 = parseInt(prompt("Betrag (in Cent):"));
+let typ_2 = prompt("Typ (Einahme oder Ausgabe):");
+console.log("Typ: ${typ_2}");
+let betrag_2 = prompt("Betrag (in Cent):");
 console.log(betrag_2);
 console.log(`Betrag: ${betrag_2} ct`);
 let datum_2 = prompt("Datum (jjjj-mm-tt):");
@@ -26,6 +26,3 @@ let einnahmen;
 let ausgaben;
 let bilanz = betrag_1 + betrag_2;
 console.log(`Bilanz: ${bilanz} ct`);
-
-let positiv = bilanz >= 0;
-console.log(`Bilanz ist positiv: ${positiv}`);
