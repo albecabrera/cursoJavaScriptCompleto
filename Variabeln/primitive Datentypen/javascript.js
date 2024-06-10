89f2e1b264cdd209f502f05ef2d8fac6661afbf1
albecabrera <albe.cabrera@yahoo.de>
2024-06-10 12:28:58 +0200

    commit -m "change 3"

---
+++ Variabeln/primitive Datentypen/javascript.js
@@ -0,0 +1,9 @@
+"user strict"; // Dies benutzt man als Standard, damit die Ausgabe in der Konsole angezeigt wird. Es wird eine Warnung angezeigt.
+
+let name = "Alberto Cabrera";
+let name2 = "Ricardo Perez";
+let code = "let test = 22;";
+
+// Numbers
+let alter = "24";
+
---
+++ Variabeln/prompt/prompt.js
@@ -0,0 +1,7 @@
+"use strict";
+
+let name = prompt("Wie ist dein Name?");
+console.log(name);
+
+let nachname = prompt("Wie ist dein Nachname?");
+console.log(nachname);
