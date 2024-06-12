bd7cab86dad50e4021efda479739b1eadc2531bc
albecabrera <albe.cabrera@yahoo.de>
2024-06-12 21:35:13 +0200

    commit -m "change 9"

---
+++ Funktionen/Funktionen mit Parametern/main.js
@@ -0,0 +1,40 @@
+"use strict"
+
+
+// const meine_funktion = function(parameter_1, parameter_2) {
+//   auszuführender Code
+// };
+
+// -> "Funktionsaufruf"
+
+// meine_funktion(p_1, p_2);
+
+// const log_die_zahl = function(a) {
+//   console.log(a)
+// };
+
+// log_die_zahl(42);
+
+const begruessung = function(vorname, nachname) {
+  console.log("Hallo ${vorname}, ${nachname}!";
+};
+
+let vor = "Alberto";
+let nach = "Cabrera";
+
+begruessung(vor, nach);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
