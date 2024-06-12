d5f842189c35cfdcf056b2657d8900e506411996
albecabrera <albe.cabrera@yahoo.de>
2024-06-12 21:08:44 +0200

    commit -m "change 8"

---
+++ Funktionen/main.js
@@ -0,0 +1,17 @@
+"use strict"
+
+// "Funktionsausdruck (anonyme Funktion)"
+// const meine_funktion = function() {
+//   auszuführender Code
+// };
+
+// -> "Funktionsaufruf"
+
+// meine_funktion();
+
+
+const log_it = function() {
+  console.log("Funktionen sind doch voll super");
+};
+
+log_it();
