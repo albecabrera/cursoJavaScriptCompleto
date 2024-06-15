b91748da2c38d955cafdd4f9664e240e18a03c49
albecabrera <albe.cabrera@yahoo.de>
2024-06-15 23:31:30 +0200

    commit -m "change 3"

--- Funktionen/Hoisting/hoisting.js
+++ Funktionen/Hoisting/hoisting.js
@@ -1,14 +1,18 @@
-"user strict"
+"user strict";
 
-let a; 
+let a;
 a = 5;
 console.log(a);
 
 let b;
-console.log(b)
+console.log(b);
 b = 10;
 
 let c;
 console.log(c);
 
-  
+let f = 25;
+if (true) {
+  let f = 40;
+  console.log(f);
+}
